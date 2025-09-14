7149bf13b09843667a8f002a96f2dd97ae2503d3
AbdElrahman33ml <12422022610559@pg.cu.edu.eg>
2025-09-14 08:32:52 +0300

    add trend analysis

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, to_date, month, year, sum, countDistinct
 
 
 spark = SparkSession.builder.appName("Retail Transactions Analysis").getOrCreate()
@@ -16,8 +16,8 @@ df_cleaned = df.filter(
 #create a temporary view for SQL queries
 df_cleaned.createOrReplaceTempView("retail")
 
-#  summarize statistics using sql
-statistics = spark.sql("""
+#  summarize Totals using sql
+Totals = spark.sql("""
     SELECT 
         COUNT(*) AS total_transactions,
         COUNT(DISTINCT InvoiceNo) AS unique_invoices,
@@ -28,4 +28,35 @@ statistics = spark.sql("""
 """)
 
 
-statistics.show()
+Totals.show()
+
+
+# trend analysis
+sales = df_cleaned.withColumn("Date", to_date("InvoiceDate"))                    .withColumn("Month", month("Date"))                    .withColumn("Year", year("Date"))
+
+# sales monthly trend
+monthly_sales = sales.groupBy("Year", "Month").agg(
+    sum(col("Quantity") * col("UnitPrice")).alias("MonthlySales")
+)
+monthly_sales.coalesce(1).write.option("header", True).csv("output/monthly_sales")
+
+# top products analysis
+top_products = sales.groupBy("Description").agg(
+    sum("Quantity").alias("TotalSold")
+).orderBy(col("TotalSold").desc())
+top_products.coalesce(1).write.option("header", True).csv("output/top_products")
+
+# customer behavior analysis
+customer_behavior = sales.groupBy("CustomerID").agg(
+    countDistinct("InvoiceNo").alias("Purchases"),
+    sum(col("Quantity") * col("UnitPrice")).alias("TotalSpent")
+)
+customer_behavior.coalesce(1).write.option("header", True).csv("output/customer_behavior")
+
+# 4. countries sales volume
+sales_by_country = sales.groupBy("Country").agg(
+    sum(col("Quantity") * col("UnitPrice")).alias("TotalSales")
+).orderBy(col("TotalSales").desc())
+sales_by_country.coalesce(1).write.option("header", True).csv("output/sales_by_country")
+
+print("Analysis complete. Results saved in the output directory.")
